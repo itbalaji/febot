fdc22dc73789ba01051e347f466e95efb2da7527
x070368 <balaji.chandran@klm.com>
2016-09-15 14:21:49 +0200

    test

--- src/main/groovy/com/herokuapp/febotnl/messenger/webhook/MessengerWebhookController.groovy
+++ src/main/groovy/com/herokuapp/febotnl/messenger/webhook/MessengerWebhookController.groovy
@@ -3,7 +3,7 @@ package com.herokuapp.febotnl.messenger.webhook
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
-import org.springframework.http.HttpStatus
+import static org.springframework.http.HttpStatus.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -26,16 +26,46 @@ class MessengerWebhookController {
                                            @RequestParam('hub.challenge') String challenge,
                                            @RequestParam('hub.verify_token') String verifyToken) {
         if ('subscribe' == mode && environment.getRequiredProperty('facebook-webhook-token') == verifyToken)
-            new ResponseEntity<String>(challenge, HttpStatus.ACCEPTED)
+            ResponseEntity.ok(challenge)
         else
-            new ResponseEntity<String>(HttpStatus.UNAUTHORIZED)
+            new ResponseEntity<String>(BAD_REQUEST)
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    ResponseEntity<String> webhook(@RequestBody body,
+    ResponseEntity webhook(@RequestBody body,
                                    @RequestParam Map<String, String> params) {
-        // TODO **Implement**
-        log.info(environment.properties.keySet().toListString())
-        new ResponseEntity<String>(HttpStatus.OK)
+        log.info('facebook-webhook-token {}', environment.containsProperty('facebook-webhook-token'))
+        log.info('facebook-page-access-token {}', environment.containsProperty('facebook-page-access-token'))
+        log.info('params {}', params)
+        if (body.object == 'page') {
+            body.entry.each {
+                def pageId = it.id
+                def timestamp = it.time
+                log.info('Received event for page {} at {}', pageId, timestamp)
+
+                it.messaging.each {event ->
+                    if (event.optin) {
+                        log.info('Received optin')
+                        // TODO **Implement**
+                    }
+                    else if (event.message) {
+                        log.info('Received message')
+                        // TODO **Implement**
+                    }
+                    else if (event.delivery) {
+                        log.info('Received delivery receipt')
+                        // TODO **Implement**
+                    }
+                    else if (event.postback) {
+                        log.info('Received post back')
+                        // TODO **Implement**
+                    }
+                    else {
+                        log.warn('Unknown messaging event {} received at webhook', event)
+                    }
+                }
+            }
+        }
+        new ResponseEntity(OK)
     }
 }
