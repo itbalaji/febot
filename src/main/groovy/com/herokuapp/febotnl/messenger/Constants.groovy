2199fc3fa64fd97ddb1468b2f0effd5e9dc6bdf4
x070368 <balaji.chandran@klm.com>
2016-09-16 16:45:26 +0200

    fix missing token param in graph api url

--- src/main/groovy/com/herokuapp/febotnl/messenger/Constants.groovy
+++ src/main/groovy/com/herokuapp/febotnl/messenger/Constants.groovy
@@ -2,5 +2,5 @@ package com.herokuapp.febotnl.messenger
 
 interface Constants {
     long STICKER_THUMBS_UP = 369239263222822
-    String GRAPH_API_URL = 'https://graph.facebook.com/v2.7/me/messages'
+    String GRAPH_API_URL = 'https://graph.facebook.com/v2.7/me/messages?access_token={access_token}'
 }
