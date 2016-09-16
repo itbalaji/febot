112c813836871eae585ec47e412121018513802f
x070368 <balaji.chandran@klm.com>
2016-09-16 13:47:34 +0200

    handle thumbs-up - missed changes

--- src/test/groovy/com/herokuapp/febotnl/FebotApplicationTests.groovy
+++ src/test/groovy/com/herokuapp/febotnl/FebotApplicationTests.groovy
@@ -3,14 +3,14 @@ package com.herokuapp.febotnl
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner)
 @SpringBootTest
+@TestPropertySource('/test.properties')
 public class FebotApplicationTests {
-
 	@Test
 	public void contextLoads() {
 	}
-
 }
