ea1640f464fa7b7607f43bcf75129fa723f9653a
Balaji Chandran <itbalaji@yahoo.co.in>
2017-08-31 23:40:18 +0200

    Febo api is now https

--- src/main/groovy/com/herokuapp/febotnl/messenger/Constants.groovy
+++ src/main/groovy/com/herokuapp/febotnl/messenger/Constants.groovy
@@ -7,6 +7,6 @@ interface Constants {
     String GOOGLE_PLACES_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={key}&location={lat},{lon}&rankby=distance&name=febo'
     String GOOGLE_TIMEZONE_URL = 'https://maps.googleapis.com/maps/api/timezone/json?key={key}&location={lat},{lon}&timestamp={timestamp}'
     String GOOGLE_MAPS_DIRECTION_URL = 'https://maps.google.com?saddr={fromLat},{fromLon}&daddr={toLat},{toLon}'
-    String FEBO_NL_URL = 'http://www.febo.nl/wp-admin/admin-ajax.php?action=store_search&lat={lat}&lng={lon}&max_results=30&radius=5'
+    String FEBO_NL_URL = 'https://www.febo.nl/wp-admin/admin-ajax.php?action=store_search&lat={lat}&lng={lon}&max_results=30&radius=5'
     String TENOR_GIF_URL = 'https://api.tenor.co/v1/search?tag={query}&key=LIVDSRZULELA&safesearch=strict'
 }
